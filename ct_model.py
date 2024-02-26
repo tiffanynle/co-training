7eeb62c6205b931aae40e769157efb79a1acef63
Tiffany Le <tiffanyle@ou.edu>
2024-02-25 23:11:19 -0600

    update predict_ddp to return probs and labels

--- ct_model.py
+++ ct_model.py
@@ -1,17 +1,11 @@
+from itertools import zip_longest
+
+import numpy as np
 import torch
+import torch.distributed as dist
 import torch.nn as nn
-import torch.optim as optim
 from torch.optim import SGD
 from torch.optim.lr_scheduler import ReduceLROnPlateau
-from torch.utils.data import DataLoader, DistributedSampler
-
-import torch.distributed as dist
-from torch.utils.data.distributed import DistributedSampler
-
-import wandb
-import numpy as np
-
-from itertools import zip_longest
 
 from utils import *
 
@@ -67,6 +61,7 @@ def test_ddp(rank, device, model, loader, loss_fn):
 def predict_ddp(world_size, device, model, loader, batch_size, num_classes):
     model.eval()
     predictions = []
+    labels = []
     softmax = torch.nn.Softmax(-1)
     with torch.no_grad():
         for X, y in loader:
@@ -81,9 +76,18 @@ def predict_ddp(world_size, device, model, loader, batch_size, num_classes):
                              dtype=torch.float16).to(device)
             pad[:output.shape[0]] = output
 
+            # same as above but we need the labels
+            label_list = [torch.full(batch_size, -1, dtype=torch.float16).to(device)]
+            pad2 = [torch.full(batch_size, -1, dtype=torch.float16).to(device)]
+            pad2[:y] = y
+
             # all-gather the full list of predictions across all processes
             dist.all_gather(tensor_list, pad)
             batch_outputs = torch.cat(tensor_list)
+
+            # all-gather the full list of labels
+            dist.all_gather(label_list, pad2)
+            batch_labels = torch.cat(label_list)
 
             # remove all rows of the tensor that contain a -1
             # (as this is not a valid value anywhere)
@@ -91,50 +95,54 @@ def predict_ddp(world_size, device, model, loader, batch_size, num_classes):
             batch_outputs = batch_outputs[mask]
             predictions.append(batch_outputs)
 
-    return torch.cat(predictions)
+            mask2 = ~(batch_labels == -1).any(-1)
+            batch_labels = batch_labels[mask]
+            labels.append(batch_labels)
+
+    return torch.cat(predictions), torch.cat(labels)
 
 
-def co_test_ddp(rank, device, models, loaders):
-    for model in models:
-        model.eval()
+# def co_test_ddp(rank, device, models, loaders):
+#     for model in models:
+#         model.eval()
 
-    iters_loaders = [iter(loader) for loader in loaders]
-    ddp_loss = torch.zeros(3).to(device)
-    with torch.no_grad():
-        pass
-    dist.all_reduce(ddp_loss, op=dist.ReduceOp.SUM)
-    test_acc = ddp_loss[1] / ddp_loss[2]
-
-    if rank == 0:
-        print('Test error: \tCo-Accuracy: {:.2f}%'
-              .format(100*test_acc))
-    
-    return test_acc
-
-# def c_test(rank, model0, model1, loader0, loader1, device):
+#     iters_loaders = [iter(loader) for loader in loaders]
 #     ddp_loss = torch.zeros(3).to(device)
-#     model0.eval()
-#     model1.eval()
 #     with torch.no_grad():
-#         for batch, ((X0, y0), (X1, y1))  in enumerate(zip(iter(loader0), iter(loader1))):
-#             X0, y0 = X0.to(device), y0.to(device)
-#             X1, y1 = X1.to(device), y1.to(device)
-
-#             output = torch.nn.Softmax(-1)(model0(X0)) * torch.nn.Softmax(-1)(model1(X1))
-
-#             ddp_loss[1] += (output.argmax(1) == y1).type(torch.float).sum().item()
-#             ddp_loss[2] += len(X0)
-    
+#         pass
 #     dist.all_reduce(ddp_loss, op=dist.ReduceOp.SUM)
-
-#     test_acc = ddp_loss[1] / ddp_loss[2] 
-#     test_loss = ddp_loss[0] / ddp_loss[2]
+#     test_acc = ddp_loss[1] / ddp_loss[2]
 
 #     if rank == 0:
 #         print('Test error: \tCo-Accuracy: {:.2f}%'
 #               .format(100*test_acc))
-
+    
 #     return test_acc
+
+def c_test(rank, model0, model1, loader0, loader1, device):
+    ddp_loss = torch.zeros(3).to(device)
+    model0.eval()
+    model1.eval()
+    with torch.no_grad():
+        for batch, ((X0, y0), (X1, y1))  in enumerate(zip(iter(loader0), iter(loader1))):
+            X0, y0 = X0.to(device), y0.to(device)
+            X1, y1 = X1.to(device), y1.to(device)
+
+            output = torch.nn.Softmax(-1)(model0(X0)) * torch.nn.Softmax(-1)(model1(X1))
+
+            ddp_loss[1] += (output.argmax(1) == y1).type(torch.float).sum().item()
+            ddp_loss[2] += len(X0)
+    
+    dist.all_reduce(ddp_loss, op=dist.ReduceOp.SUM)
+
+    test_acc = ddp_loss[1] / ddp_loss[2] 
+    test_loss = ddp_loss[0] / ddp_loss[2]
+
+    if rank == 0:
+        print('Test error: \tCo-Accuracy: {:.2f}%'
+              .format(100*test_acc))
+
+    return test_acc
 
 
 def get_topk(prediction, k):
@@ -158,28 +166,17 @@ def merge_wandb_logs(iteration, epochs, wandb_logs) -> list:
     return logs_with_steps
 
 
-class CoTrainingModel(torch.nn.Module):
-    def __init__(self, rank, world_size, models) -> None:
-        super().__init__()
+class CoTrainingModel:
+    def __init__(self, 
+                 rank: int, 
+                 world_size: int, 
+                 models: list) -> None:
         self.rank = rank
         self.world_size = world_size
         self.models = models
         self.logs = []
 
-    def forward(self, x) -> float:
-        pass
-
-    # update labeled and unlabeled datasets
-    def update(self,
-               device,
-               train_views, unlbl_views,
-               num_classes, k_total,
-               batch_size=64) -> None:
-        assert len(train_views) == len(unlbl_views) == len(self.models), \
-                "number of views and number of models must be the same -- \
-                got: train {}, unlabeled {}, models {}".format(
-                    len(train_views), len(unlbl_views), len(self.models))
-
+    def predict(self, device, unlbl_views, num_classes, batch_size):
         samplers_unlbl = []
         loaders_unlbl = []
         for i in range(len(unlbl_views)):
@@ -197,16 +194,30 @@ class CoTrainingModel(torch.nn.Module):
         # make predictions for everything in unlabeled set, for all models
         preds_softmax = []
         for i, model in enumerate(self.models):
-            pred = predict_ddp(self.world_size, device, 
+            pred, labels = predict_ddp(self.world_size, device, 
                                model, loaders_unlbl[i], 
                                batch_size, 
                                num_classes)[:len(unlbl_views[i])]
             # ensure all processes agree on the list of predictions
             dist.broadcast(pred, 0)
             preds_softmax.append(pred)
+        
+        dist.broadcast(labels)
 
         # num views x num instances x num classes
         preds_softmax = torch.stack(preds_softmax)
+
+        return preds_softmax, labels
+
+    def update(self,
+               preds_softmax: torch.Tensor,
+               train_views: list, 
+               unlbl_views: list,
+               k_total: float) -> None:
+        msg = (f'number of views and number of models must be the same -- '
+               f'got train: {len(train_views)}, unlabeled: {len(unlbl_views)}, '
+               f'models: {len(self.models)}')
+        assert len(train_views) == len(unlbl_views) == len(self.models), msg
 
         # assuming that k is # of total dataset to bring in
         # we'll have each model take in top-(k // len(models)) predictions
@@ -238,9 +249,9 @@ class CoTrainingModel(torch.nn.Module):
 
     # 2 views, no ensembling
     def _resolve_conflicts_twoview(self, lbls_topk, idxes_topk):
-        assert len(lbls_topk) == len(idxes_topk) == 2, \
-                "expected predictions for 2 views, got: labels {}, indexes {}" \
-                .format(len(lbls_topk), len(idxes_topk))
+        msg = (f'expected predictions for 2 views, got: '
+               f'labels {len(lbls_topk)}, indices {len(idxes_topk)}')
+        assert len(lbls_topk) == len(idxes_topk) == 2, msg
 
         inter, idx_inter0, idx_inter1 = np.intersect1d(idxes_topk[0],
                                                        idxes_topk[1],
@@ -335,6 +346,7 @@ class CoTrainingModel(torch.nn.Module):
             # update mask to remove now-pseudolabeled instances (later)
             mask[idx_topk] = False
 
+        # remove pseudolabeled instances from unlabeled set
         for i in range(num_views):
             samples_unlbl = np.stack([np.array(a) 
                                       for a in unlbl_views[i].samples])
@@ -348,31 +360,29 @@ class CoTrainingModel(torch.nn.Module):
 
     # one full pass of co-training without dataset updates
     def train(self,
-              device,
-              iteration, 
-              epochs,
-              states,
-              train_views,
-              loaders_val,
-              loaders_test, 
-              batch_size=64,
-              optimizer=SGD,
-              optimizer_kwargs={'lr':1e-3,
-                                'momentum': 0.9},
-              stopper_kwargs={'metric': 'accuracy',
-                              'patience': 32,
-                              'min_delta': 1e-3},
-              lr_scheduler=ReduceLROnPlateau) -> None:
-        assert len(train_views) == len(loaders_val) == len(loaders_test) == len(self.models), \
-                "number of views and number of models must be the same -- \
-                got: train {}, val loaders {}, test loaders {}, models {}".format(
-                    len(train_views), len(loaders_val), len(loaders_test), len(self.models))
+              device: torch.device,
+              iteration: int, 
+              epochs: int,
+              states: dict,
+              train_views: list,
+              val_views: list,
+              test_views: list,
+              batch_size: int = 64,
+              optimizer: torch.optim.Optimizer = SGD,
+              optimizer_kwargs: dict = {'lr':1e-3,
+                                        'momentum': 0.9},
+              stopper_kwargs: dict = {'metric': 'accuracy',
+                                      'patience': 32,
+                                      'min_delta': 1e-3},
+              lr_scheduler: torch.optim.lr_scheduler = ReduceLROnPlateau) -> None:
+        msg = (f'number of views and number of models must be the same -- '
+               f'got train: {len(train_views)}, val: {len(val_views)}, '
+               f'test: {len(test_views)}, models: {len(self.models)}')
+        assert len(train_views) == len(val_views) == len(test_views) == len(self.models), msg
 
         optimizers = []
         schedulers = []
         stoppers = []
-        samplers_train = []
-        loaders_train = []
 
         for i in range(len(self.models)):
             stoppers.append(EarlyStopper(**stopper_kwargs))
@@ -386,14 +396,17 @@ class CoTrainingModel(torch.nn.Module):
             for i, opt in enumerate(optimizers):
                 schedulers.append(lr_scheduler(opt)) 
 
-        for i in range(len(train_views)):
-            sampler_train, loader_train = create_sampler_loader(self.rank, 
-                                                                self.world_size,
-                                                                train_views[i], 
-                                                                batch_size,
+        samplers_train, loaders_train = create_samplers_loaders(self.rank, self.world_size,
+                                                                train_views, batch_size, 
                                                                 persistent_workers=True)
-            samplers_train.append(sampler_train)
-            loaders_train.append(loader_train)
+        
+        samplers_val, loaders_val = create_samplers_loaders(self.rank, self.world_size,
+                                                            val_views, batch_size, 
+                                                            persistent_workers=True)
+        
+        samplers_test, loaders_test = create_samplers_loaders(self.rank, self.world_size,
+                                                              test_views, batch_size, 
+                                                              persistent_workers=True)
 
         loss_fn = nn.CrossEntropyLoss()
 
@@ -415,13 +428,6 @@ class CoTrainingModel(torch.nn.Module):
                                          loaders_val[i], loss_fn)
                 test_acc, test_loss = test_ddp(self.rank, device, self.models[i],
                                                loaders_test[i], loss_fn)
-                
-                # if rank == 0:
-                #     wandb.log({f'train_acc{i}': train_acc,
-                #                f'train_loss{i}': train_loss,
-                #                f'val_acc{i}': val_acc,
-                #                f'val_loss{i}': val_loss},
-                #                step=step)
 
                 stoppers[i].step(val_acc, val_loss)
                 if stoppers[i].epochs_since_improvement == 0:
@@ -449,6 +455,7 @@ class CoTrainingModel(torch.nn.Module):
             loaders_train[i]._iterator._shutdown_workers()
             loaders_val[i]._iterator._shutdown_workers()
             loaders_test[i]._iterator._shutdown_workers()
+
             iteration_logs.append(model_logs)
 
         self.logs += merge_wandb_logs(iteration, epochs, iteration_logs)
